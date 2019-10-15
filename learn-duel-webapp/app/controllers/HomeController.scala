1165d42e4cc199d7038030c3511abf9dbcbcb69e
Lionel Kornberger <lio_ko@protonmail.com>
2019-10-15 08:59:21 +0200

    Added rough di

--- learn-duel-webapp/app/controllers/HomeController.scala
+++ learn-duel-webapp/app/controllers/HomeController.scala
@@ -2,7 +2,9 @@ package controllers
 
 import javax.inject._
 import play.api._
+
 import play.api.mvc._
+import de.htwg.se.learn_duel.controller.Controller
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -21,4 +23,9 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
+//  def menu()= Action {
+//    val menuAsText = controllerServer.menuToText
+//    Ok(menuAsText)
+//  }
 }
