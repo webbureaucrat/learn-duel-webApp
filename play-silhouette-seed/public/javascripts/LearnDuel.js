06d1c913b61ce0f658d69409788cb85e24347e52
Michael S <michael_scheer1993@web.de>
2020-01-14 23:48:55 +0100

    Init

---
+++ play-silhouette-seed/public/javascripts/LearnDuel.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+    // setTimer(60);
+    $("#openSources").click(function () {
+        window.open("https://github.com/webbureaucrat/learn-duel-webApp", '_blank');
+    })
+};
+
+// function setTimer(seconds) {
+//     // jquery selection does not work
+//     // if (window.location.pathname === '/start') {
+//         document.querySelector('#countdown').textContent = seconds;
+//     // }
+// }
+
+function onAnswerChosen(pid) {
+    $.ajax({
+        url: "/onAnswerChosen?position=" + pid,
+        type: 'GET',
+        success: function (results) {
+            if (typeof (results) == "object") {
+            } else {
+                $(".row").replaceWith(results);
+            }
+        },
+        async: true,
+        error: function (results) {
+            alert('Make call failed');
+        }
+    });
+}
+
+function displayQuestions(question) {
+    $(".text-center").text(question.text);
+    $("#answer0").html(question.answers[0].text);
+    $("#answer1").html(question.answers[1].text);
+    $("#answer2").html(question.answers[2].text);
+    $("#answer3").html(question.answers[3].text);
+}
+
+function startGame() {
+    $.ajax({
+        url: "/questions",
+        type: 'GET',
+        success: function (results) {
+        },
+        async: true,
+        error: function (results) {
+            alert('Make call failed');
+        }
+    });
+
+}
+
+// $(document).ready(function () {
+//     var app = new Vue({
+//         el: '#app',
+//         render: function (html) {
+//             return html(MainContent.default, {
+//                 props: {
+//                     show: "about"
+//                 }
+//             });
+//         }
+//     })
+// });
+
+export {displayQuestions, startGame, onAnswerChosen};
